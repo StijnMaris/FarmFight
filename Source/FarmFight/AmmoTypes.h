cd4aad6ca3245c3611055c73b6b25aebf549c80d
Shake <robin.smekens.930@gmail.com>
2020-10-17 16:58:49 +0200

    restructuring ammo types system

---
+++ Source/FarmFight/AmmoTypes.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Engine/DataTable.h"
+#include "AmmoTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAmmoTypeDatatableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 StartingAmmoCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 MaxAmmoutCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText AmmoName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLinearColor AmmoColor;
+};
